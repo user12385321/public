618d8dd0db6c9013fd9fa68b927c2723ee8a2fd8
user12385321 <kukulo.stas@gmail.com>
2024-12-08 01:22:59 +0300

    Update calculon.ps1

--- calculon.ps1
+++ calculon.ps1
@@ -1,14 +1,6 @@
 <#
 .SYNOPSIS
 	Launches the calculator application
-.DESCRIPTION
-	This PowerShell script launches the calculator application.
-.EXAMPLE
-	PS> ./open-calculator
-.LINK
-	https://github.com/fleschutz/PowerShell
-.NOTES
-	Author: Markus Fleschutz | License: CC0
 #>
 
 try {
